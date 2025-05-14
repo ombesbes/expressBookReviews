b863628483fa6228fea4fc84dae1363b2f219d27
Omar Besbes <ombesbes@gmail.com>
2025-05-14 07:04:01 -0400

    updated general.js task 1 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,11 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/',async (req, res) => {
+    res.send(JSON.stringify(books,null,4));
+   
+  });
+  
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
