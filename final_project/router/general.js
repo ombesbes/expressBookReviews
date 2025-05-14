34f51d5993e179bea0c927ac243677b2b6c5cc9e
Omar Besbes <ombesbes@gmail.com>
2025-05-14 11:47:06 -0400

    task2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,10 +18,30 @@ public_users.get('/',async (req, res) => {
   
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', (req, res) =>{
+    
+    const ISBN = req.params.isbn;
+    const booksBasedOnIsbn = (ISBN) => {
+        return new Promise((resolve,reject) =>{
+          setTimeout(() =>{
+            const book = books.find((b) => b.isbn === ISBN);
+            if(book){
+              resolve(book);
+            }else{
+              reject(new Error("Book not found"));
+            }},1000);
+        });
+    
+            
+    }
+    booksBasedOnIsbn(ISBN).then((book) =>{
+      res.json(book);
+    }).catch((err)=>{
+      res.status(400).json({error:"Book not found"})
+    });
+     res.send(books[ISBN]);    
+   
+   });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
