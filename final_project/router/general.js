ba8d175f416a1dc6afb8eb6178b0b375c0830fc2
Omar Besbes <ombesbes@gmail.com>
2025-05-15 06:34:34 -0400

    task6 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,29 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+
+    const username = req.body.username;
+    const password = req.body.password;
+    if(username&&password){
+        const present = users.filter((user)=> user.username === username)
+        if(present.length===0){
+            users.push({"username":req.body.username,"password":req.body.password});
+            return res.status(201).json({message:"User successfully registered."})
+        }
+        else{
+          return res.status(400).json({message:"Already exists"})
+        }
+    }
+    else if(!username && !password){
+      return res.status(400).json({message:"Bad request"})
+    }
+    else if(!username || !password){
+      return res.status(400).json({message:"Check username and password"})
+    }  
+  
+   
+  });
+
 
 // Get the book list available in the shop
 public_users.get('/',async (req, res) => {
@@ -16,6 +36,7 @@ public_users.get('/',async (req, res) => {
    
   });
   
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', (req, res) =>{
@@ -42,6 +63,8 @@ public_users.get('/isbn/:isbn', (req, res) =>{
      res.send(books[ISBN]);    
    
    });
+
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -72,6 +95,7 @@ public_users.get('/title/:title',function (req, res) {
    }
     res.send(JSON.stringify(new_books));
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
